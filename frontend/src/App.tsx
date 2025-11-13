7c757d1de800e82d1a114fddd61877600dd34f56
Haleem Akmal <haleemakmal48@gmail.com>
2025-11-14 03:04:54 +0530

    feat: Configure Tailwind CSS for Vite + React project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+// frontend/src/App.tsx
+
+function App() {
+  return (
+    // 1. These classNames come from Tailwind CSS
+    <div className="flex h-screen items-center justify-center bg-gray-50">
+      <h1 className="text-3xl font-bold text-red underline">
+        Hello Tailwind!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
